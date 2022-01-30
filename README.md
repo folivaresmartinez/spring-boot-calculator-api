06803f3baaacdfb87f1b19c046974c717afcace2
folivaresmartinez <folivaresmartinez@gmail.com>
2022-01-30 13:12:51 +0100

    First Commit. Creation project

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Iteriam-Test
+# spring-boot-calculator api-application
+Iteriam Calculator Test Application
+
+# Port where the microservice publish the API Methods
+Microservice Port: 8081
+
+# Compile and run the microservice
+mvn clean install
+
+mvn spring-boot:run
+
+# Run Test
+mvn test
+
+
---
+++ src/main/java/com/iteriam/SpringBootIteriamCalculatorApplication.java
@@ -0,0 +1,20 @@
+package com.iteriam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Main class for the SpringBootIteriamCalculatorApplication
+ * @author Fernando Olivares
+ * @version 1.0
+ *
+ */
+@SpringBootApplication
+public class SpringBootIteriamCalculatorApplication 
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringBootIteriamCalculatorApplication.class, args);
+	}
+}
