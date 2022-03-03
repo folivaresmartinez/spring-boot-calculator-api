f5c49c2149ad5ceea168c921f3b2119db2cd8a6c
OPTIMISSA\fernando.olivares <folivaresmartinez@gmail.com>
2022-03-03 14:36:54 +0100

    Commit 2

--- src/main/java/com/iteriam/calculator/model/ValuesDtO.java
+++ src/main/java/com/iteriam/calculator/model/ValuesDtO.java
@@ -14,19 +14,19 @@ import lombok.NoArgsConstructor;
 public class ValuesDtO {
 
 	/**
-	 * First Operator Value 
+	 * First Operator Value .
 	 */
 	private Optional<String> firstValue;
 	
 	/**
-	 * Second Operator Value
+	 * Second Operator Value.
 	 */
 	private Optional<String> secondValue;
 	
 	/**
-	 * Default Constructor
-	 * @param firstValue: First Operator Value
-	 * @param secondValue: Second Operator Value
+	 * Default Constructor.
+	 * @param firstValue: First Operator Value.
+	 * @param secondValue: Second Operator Value.
 	 */
 	public ValuesDtO(Optional<String> firstValue, Optional<String> secondValue) {
 		this.firstValue = firstValue.filter(str -> str.matches("(^\\d*\\.?\\d*[0-9]+\\d*$)|(^[0-9]+\\.?\\d*$)"));
