edde610afa6d0b1b495f15eef9de3c1f6f95369b
OPTIMISSA\fernando.olivares <folivaresmartinez@gmail.com>
2022-03-03 15:45:57 +0100

    Commit 1

--- src/main/java/com/iteriam/calculator/model/ValuesDtO.java
+++ src/main/java/com/iteriam/calculator/model/ValuesDtO.java
@@ -25,8 +25,8 @@ public class ValuesDtO {
 	
 	/**
 	 * Default Constructor.
-	 * @param firstValue: First Operator Value.
-	 * @param secondValue: Second Operator Value.
+	 * @param firstValue: First Operator Value
+	 * @param secondValue: Second Operator Value
 	 */
 	public ValuesDtO(Optional<String> firstValue, Optional<String> secondValue) {
 		this.firstValue = firstValue.filter(str -> str.matches("(^\\d*\\.?\\d*[0-9]+\\d*$)|(^[0-9]+\\.?\\d*$)"));
